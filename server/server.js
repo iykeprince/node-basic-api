69b029d17a37c54c0a475c84512e1dd946422ec1
Piemonte Spectre <iykeprince48@gmail.com>
2021-05-31 20:22:52 -0700

    added server for GETting individual object by id

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser')
 const { mongoose } = require('./db/mongoose')
 const { Todo } = require('./models/todo')
 const { User}  = require('./models/user')
+const { ObjectID } = require('bson')
 
 const app = express();
 
@@ -32,6 +33,21 @@ app.get('/todos', (req, res) => {
             res.status(400).send(error);
         }
     );
+});
+
+app.get('/todos/:id', (req, res) => {
+    const id = req.params.id;
+
+    if(!ObjectID.isValid(id)){
+        return res.status(404).send()
+    }
+
+    Todo.findById(id).then(todo => {
+        if(!todo){
+            return res.status(404).send()
+        }
+        res.send({todo})
+    }, (e) => res.status(400).send())
 })
 
 app.listen(3000, () => console.log(`Started on port 3000`))
