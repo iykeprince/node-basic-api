1fc7dbfa5eadfbefee78485e1cee35212b2f30d4
Piemonte Spectre <iykeprince48@gmail.com>
2021-06-07 00:42:12 +0100

    Hashed password

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,14 +1,26 @@
-const {SHA256} = require('crypto-js');
-const jwt = require('jsonwebtoken');
+const bcryptjs = require('bcryptjs')
 
-const data = {
-    id: 10
-}
-const token = jwt.sign(data, '123abc');
-console.log(token)
+var password = '123abc!';
+bcryptjs.genSalt(10, (err, salt) => {
+    bcryptjs.hash(password, salt, (err, hash) =>{
+        console.log(hash);
+    });
+});
+var hashedPassword = '$2a$10$U46jceH6sKmo/9xwoDYVI.D1RT8mz7aGaMdMKlT2m86jB7nG6yere';
+bcryptjs.compare(password, hashedPassword, (err, res) => {
+    console.log(res)
+})
+// const {SHA256} = require('crypto-js');
+// const jwt = require('jsonwebtoken');
 
-var decoded = jwt.verify(token, '123abc')
-console.log('decoded', decoded)
+// const data = {
+//     id: 10
+// }
+// const token = jwt.sign(data, '123abc');
+// console.log(token)
+
+// var decoded = jwt.verify(token, '123abc')
+// console.log('decoded', decoded)
 // const message = 'I am user number 3';
 // var hash = SHA256(message).toString();
 
